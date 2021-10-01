d7412cbe8c3cb200dd6a5008cf7432fa3ae8f668
Alexey <alexfilonov2015@yandex.ru>
2021-10-01 17:51:17 +0300

    fixed orientation

--- app/src/main/java/com/example/rickandmortycharacters/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortycharacters/MainActivity.kt
@@ -4,16 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
-import com.example.rickandmortycharacters.viewmodels.CharactersPageViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
-class ListActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list)
-        supportFragmentManager.commit {
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) supportFragmentManager.commit {
             setReorderingAllowed(true)
             add<ListFragment>(R.id.fragment_container)
         }
